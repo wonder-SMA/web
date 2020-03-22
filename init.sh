9d1b96f24c41f31fc81738817787557b15954d27
wonder-SMA <61292853+wonder-SMA@users.noreply.github.com>
2020-03-23 01:37:38 +0400

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,4 +1,2 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/default
-sudo /etc/init.d/gunicorn restart
