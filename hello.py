71ddb461bb6cf1d34e851644777ae785e26bacf8
wonder-SMA <61292853+wonder-SMA@users.noreply.github.com>
2020-03-23 02:03:54 +0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 def app(environ, start_response):
-    data = '\n'.join(environ.get('QUERY_STRING').split('&'))
+    data = ('\n'.join(environ.get('QUERY_STRING').split('&'))).encode('utf-8')
     status = '200 OK'
     response_headers = [
 	('Content-type', 'text/plain'),
