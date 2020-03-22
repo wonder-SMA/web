559f35d82bd83562997dd41500c9fb3102b11a8b
wonder-SMA <61292853+wonder-SMA@users.noreply.github.com>
2020-03-23 02:21:29 +0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,9 +1,10 @@
 def app(environ, start_response):
-    data = ('\n'.join(environ.get('QUERY_STRING').split('&'))).encode('utf-8')
     status = '200 OK'
+    data = environ['QUERY_STRING'].split("&")
+    data = [item+"\r\n" for item in resp]
     response_headers = [
 	('Content-type', 'text/plain'),
 	('Content-Length', str(len(data)))
     ]
     start_response(status, response_headers)
-    return iter([data])
+    return resp
