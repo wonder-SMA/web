32655d138292d07419e4b6560b8dea357f57c8e7
wonder-SMA <61292853+wonder-SMA@users.noreply.github.com>
2020-04-06 18:17:26 +0400

    Update urls.py

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include, url
+from django.urls import path, include
 admin.autodiscover()
 
 urlpatterns = [
