75d842bf5642899ad5a8d13158265b49a4f9623e
wonder-SMA <61292853+wonder-SMA@users.noreply.github.com>
2020-04-06 18:16:53 +0400

    Update urls.py

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -2,11 +2,11 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-        path('', views.question_list, name='new_question_list'),
-        path('login/', views.login, name='login'),
-        path('signup/', views.signup, name='signup'),
-        path('question/<int:pk>/', views.question_detail, name='question_detail'),
-        path('ask/', views.post_ask, name='new_ask'),
-        path('popular/', views.popular_question_list, name='popular_question_list'),
-        path('new/', views.test,)
-        ]
+        path('', views.test),
+        path('login/', views.test),
+        path('signup/', views.test),
+        path('question/<int:pk>/', views.test),
+        path('ask/', views.test),
+        path('popular/', views.test),
+        path('new/', views.test),
+]
