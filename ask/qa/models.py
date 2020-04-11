1b7461a696ba2a2a474c2740cb4807f7e52e10e6
wonder-SMA <wonder2012mail@gmail.com>
2020-04-11 23:52:27 +0400

    111

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -3,4 +3,25 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
+
+class QuestionManager(models.Manager):
+    def new(self):
+        return self.order_by('-added_at')
+    def popular(self):
+        return self.order_by('-rating')
+
+class Question(models.Model):
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    added_at = models.DateTimeField(auto_now_add=True)
+    rating = models.IntegerField(default=0)
+    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    likes = models.ManyToManyField(User, related_name='question_like_user')
+    objects = QuestionManager()
+
+class Answer(models.Model):
+    text = models.TextField()
+    added_at = models.DateTimeField(auto_now_add=True)
+    question = models.ForeignKey(Question, null=True, on_delete=models.SET_NULL)
+    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
