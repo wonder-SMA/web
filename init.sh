43bf1645e3fbc06e0c4231ab9bef0dbd6b935903
wonder-SMA <wonder2012mail@gmail.com>
2020-04-18 21:31:47 +0400

    111

--- init.sh
+++ init.sh
@@ -1,7 +1,6 @@
 sudo rm /etc/nginx/sites-enabled/default
 echo "REMOVE default config"
-sudo cp ./etc/server.conf /etc/nginx/sites-enabled/
+sudo cp ./etc/nginx.conf /etc/nginx/sites-enabled/
 echo "COPY CONFIG"
 sudo service nginx reload
 echo "NGINX RELOADED"
-cd ask && gunicorn --bind=0.0.0.0:8000 --log-level=debug --workers=2 --timeout=15 ask.wsgi
