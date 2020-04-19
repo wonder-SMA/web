bf57bbb8f94b68ed5359ccccfa8680fb10fcc728
wonder-SMA <wonder2012mail@gmail.com>
2020-04-19 20:13:44 +0400

    111

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -7,44 +7,24 @@ from .models import Question
 def test(request, *args, **kwargs):
     return HttpResponse('OK')
 
-@require_GET
 def question_details(request, pk):
-    question = get_object_or_404(Question, pk=pk)
+    question = get_object_or_404(Question, id=pk)
     answers = question.answer_set.all()
     return render(request, 'question_details.html', {'question': question, 'answers': answers})
 
-def question_list(request):
-    questions = Question.objects.new()
-    page = paginate(request, questions)
-    page.baseurl = '/?page='
-    return render(request, 'index.html', {
-        'questions':  page.object_list,
-        'page':       page,
-    })
 
+def question_list(request):
+    objects = Question.objects.new()
+    limit = 10
+    paginator = Paginator(objects, limit)
+    page_number = request.GET.get('page', 1)
+    questions = paginator.get_page(page_number)
+    return render(request, 'index.html', {'questions': questions})
 
 def popular_question_list(request):
-    questions = Question.objects.popular()
-    page = paginate(request, questions)
-    page.baseurl = '/popular/?page='
-    return render(request, 'index.html', {
-        'questions':  page.object_list,
-        'page':       page,
-    })
-
-
-#def question_list(request):
- #   objects = Question.objects.new()
-  #  limit = 10
-   # paginator = Paginator(objects, limit)
-    #page_number = request.GET.get('page', 1)
-    #questions = paginator.get_page(page_number)
-    #return render(request, 'index.html', {'questions': questions})
-
-#def popular_question_list(request):
- #   objects = Question.objects.popular()
-  #  limit = 10
-   # paginator = Paginator(objects, limit)
-    #page_number = request.GET.get('page', 1)
-    #questions = paginator.get_page(page_number)
-    #return render(request, 'index.html', {'questions': questions})
+    objects = Question.objects.popular()
+    limit = 10
+    paginator = Paginator(objects, limit)
+    page_number = request.GET.get('page', 1)
+    questions = paginator.get_page(page_number)
+    return render(request, 'index.html', {'questions': questions})
